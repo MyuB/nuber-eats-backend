8bd1cc70015bc022a66509eb131ccf7ae4ef82f2
MyuB <mjycoco0@naver.com>
2023-09-04 21:00:57 +0900

    feat: validationPipe를 통해 type checking 추가

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
+import { IsBoolean, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan?: boolean;
+
   @Field(() => String)
+  @IsString()
   address: string;
+
   @Field(() => String)
   ownersName: string;
 }
